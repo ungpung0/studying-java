c77ff68a937540508d32768f130e3e9e86850fd7
ungpung0 <ungpung0@gmail.com>
2022-07-17 17:22:25 +0900

    baekjoon java study, java description ch1 complete. (22-07-13)

---
+++ baekjoon/src/basic/stringprocess/answer09/Main.java
@@ -0,0 +1,26 @@
+package basic.stringprocess.answer09;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String sentense = bufferedReader.readLine();
+        int count = 0;
+        for(int i = 0; i < sentense.length(); i++) {
+            char current = sentense.charAt(i);
+            for(int j = 0; j < array.length; j++) {
+                if(current != )
+            }
+        }
+
+        bufferedWriter.write(String.valueOf(count));
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
+
